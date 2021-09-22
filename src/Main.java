fd976952820fb336767af033efc927cfea06ce93
SebbeKYH <sebastian.andersson2@student.kyh.se>
2021-09-22 14:07:10 +0200

    Exercise 2

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,18 @@ package PACKAGE_NAME;
 
 public class Main {
     system.out.println("Sebastian branch");
+
+
+
+
+
+    // Exercise 2
+    int number[]={1,4,6,8,4,3};
+    int sum =0;
+    int i=0;
+        for(i;i<number.length;i++){
+            sum+=i;
+        System.out.println("The sum is"+sum);
+    }
 }
+
