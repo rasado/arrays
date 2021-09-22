e824fa8eced0656be537c612e2f3815172b55b08
SebbeKYH <sebastian.andersson2@student.kyh.se>
2021-09-22 14:55:20 +0200

    Exercise 6, not finished

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        // Exercise 2
+        // Exercise 2 Write a Java program på sum values of an array
         System.out.println("Varför fungerar det inte?");
         int number[] = {1, 4, 6, 8, 4, 3};
         int sum = 0;
@@ -10,7 +10,7 @@ public class Main {
             sum+=i;
             System.out.println("The sum is " + sum);
 
-            //Exercise 4
+            //Exercise 4 Write a Java program to calculate the average value of array elements
         double averageNumber[]={5,6,7,8,9,4,2};
         double sumAverage=0;
         double sumAverageFinal=0;
@@ -18,5 +18,15 @@ public class Main {
             sumAverage+=i;
             sumAverageFinal=sumAverage/averageNumber.length;
         System.out.println("The average sum is " +sumAverageFinal);
+
+        //Exercise 6 Write a Java program to find the index of an array element
+        //String someElements[]={"Sebastian", "utomhus", "cykel", "träd", "park"};
+        //int length = someElements.length;
+        //int i=0;
+        //while(i<length){
+        //    if(someElements[i]==t)
+        //}
+
+        }
     }
-}
+
