04bc51503c19a26efd4df3276b5f3a10f19bf5ec
SebbeKYH <sebastian.andersson2@student.kyh.se>
2021-09-22 14:33:02 +0200

    Exercise 4

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,22 @@
-package PACKAGE_NAME;
+
 
 public class Main {
-    system.out.println("Sebastian branch");
-
-
-
-
-
-    // Exercise 2
-    int number[]={1,4,6,8,4,3};
-    int sum =0;
-    int i=0;
-        for(i;i<number.length;i++){
+    public static void main(String[] args) {
+        // Exercise 2
+        System.out.println("Varför fungerar det inte?");
+        int number[] = {1, 4, 6, 8, 4, 3};
+        int sum = 0;
+        for (int i : number)
             sum+=i;
-        System.out.println("The sum is"+sum);
+            System.out.println("The sum is " + sum);
+
+            //Exercise 4
+        double averageNumber[]={5,6,7,8,9,4,2};
+        double sumAverage=0;
+        double sumAverageFinal=0;
+        for(double i : averageNumber)
+            sumAverage+=i;
+            sumAverageFinal=sumAverage/averageNumber.length;
+        System.out.println("The average sum is " +sumAverageFinal);
     }
 }
-
