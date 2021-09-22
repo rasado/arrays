bee2f2a815f75e2e27b522e460dd6ea14f99aed6
Rasmus Adolfsson <rasmus.adolfsson@student.kyh.se>
2021-09-22 13:53:46 +0200

    master sout change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Detta är sebastian branchen");
+        System.out.println("Detta är master branchen");
     }
 }
