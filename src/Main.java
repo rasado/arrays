d4d818ef692206068fca0ad6b26f4a9f7d7156a4
SebbeKYH <sebastian.andersson2@student.kyh.se>
2021-09-22 13:34:33 +0200

    1334

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-package PACKAGE_NAME;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Sebastians branch");
-    }
+package PACKAGE_NAME;
+
+public class Main {
 }
